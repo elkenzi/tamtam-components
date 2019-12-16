b36334e28905b1dc735fb9529bf624e18bd6a4ef
ramrami <ramrami.mohamed@gmail.com>
2019-12-16 07:31:02 +0100

    Add index.js

--- src/components/Header/Header.stories.js
+++ src/components/Header/Header.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
-import Header from './Header';
+import { Header } from './Header';
 
 export const actions = {
     onLanguageChange: action('onLanguageChange'),
