fda2bd4af1c32011db01db6b2c7912ae14263140
Mohamed Amine EL KENZI <elkenziamine@gmail.com>
2021-02-01 17:59:54 +0100

    Remove foundation js & jquery

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 // TODO remove foundation js
-import "foundation-sites/dist/js/plugins/foundation.core";
-import "foundation-sites/dist/js/plugins/foundation.util.keyboard.js";
-import "foundation-sites/dist/js/plugins/foundation.util.triggers.js";
-import "foundation-sites/dist/js/plugins/foundation.util.mediaQuery.js";
-import "foundation-sites/dist/js/plugins/foundation.util.motion.js";
-import "foundation-sites/dist/js/plugins/foundation.util.box.js";
-import "foundation-sites/dist/js/plugins/foundation.reveal.js";
-import "foundation-sites/dist/js/plugins/foundation.offcanvas.js";
-import "foundation-sites/dist/js/plugins/foundation.tooltip.js";
+// import "foundation-sites/dist/js/plugins/foundation.core";
+// import "foundation-sites/dist/js/plugins/foundation.util.keyboard.js";
+// import "foundation-sites/dist/js/plugins/foundation.util.triggers.js";
+// import "foundation-sites/dist/js/plugins/foundation.util.mediaQuery.js";
+// import "foundation-sites/dist/js/plugins/foundation.util.motion.js";
+// import "foundation-sites/dist/js/plugins/foundation.util.box.js";
+// import "foundation-sites/dist/js/plugins/foundation.reveal.js";
+// import "foundation-sites/dist/js/plugins/foundation.offcanvas.js";
+// import "foundation-sites/dist/js/plugins/foundation.tooltip.js";
 
 import "./styles/global.scss";
 
