26b2e4508a4a4563d70604fdbeb410d6f2fe5866
amine <elkenziamine@gmail.com>
2021-03-22 11:25:37 +0100

    Saving with spinner

--- src/components/Article/ArticleList.stories.js
+++ src/components/Article/ArticleList.stories.js
@@ -36,7 +36,7 @@ export const Default = () => (
     saveFavorite={(articleId) => console.log(articleId)}
     onLike={(articleId, like) => console.log(articleId, like)}
     openModal={(article) => console.log(article)}
-    isSavingFavorite={true}
+    isSavingFavorite={false}
     isSavingLike={false}
     isSavingDislike={false}
   />
